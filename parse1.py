a00ac40dd2c80260aaf03e53b2acea6396b1731f
antuanf1 <amartinezalf@yahoo.es>
2021-04-04 16:34:10 +0200

    parsing pilots

--- parse1.py
+++ parse1.py
@@ -18,10 +18,26 @@ def parse1():
     links:list = soup.find_all('a')
     print("links encontrados:" + str(len(links)))
     i:int = 1
+    rowCount: int = 1
+    maxRowElements: int = 3
+    name:str = ""
+    country:str = ""
+    scuderia:str = ""
     for link in links:
-        print(link.get('class'))
-        print(link.string)
-        
+        if (type(link.get('class')) is not type(None)):
+            if (link.get('class')[0] == '_1TRrV'):
+                if rowCount == 1:
+                    name = print(link.string)
+                elif rowCount == 2:
+                    country = print(link.string)
+                elif rowCount == 3:
+                    scuderia = print(link.string)
+                    print (name + ":" + country + ":" + scuderia)
+                    rowCount = 0
+        print(i)
+        print(rowCount)
+        i = i + 1
+        rowCount = rowCount + 1
 
 if __name__ == "__main__":
     main()  
