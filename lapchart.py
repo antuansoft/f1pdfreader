92f69c979670414ab8627b044625d60151648b1c
antuanf1 <amartinezalf@yahoo.es>
2022-09-12 13:52:47 +0200

    Changed Lap to Number

--- lapchart.py
+++ lapchart.py
@@ -1,6 +1,7 @@
 # class lapchart
 
 import json
+from tokenize import Number
 
 
 class LapChart:
@@ -15,6 +16,15 @@ class LapChart:
     
     def toJson(self)-> str:
         lapchart_str: str = ""
-        lapchart_str += "\""+ self.lap +"\":"
+        lapchart_str += "\""+ self.changeLapToNumber(self.lap) +"\":"
         lapchart_str +=json.dumps(self.cars)
         return lapchart_str
+
+
+    def changeLapToNumber(self,text)->str:
+        if (text == "Grid"):
+            return "0"
+        elif (text.find("Lap") > -1 ):
+            return text[4:]
+        else:
+            return text
